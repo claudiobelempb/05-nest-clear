30942f8341da7b7f8cbdedf0c43b4493e8c6aad8
claudiobelempb <claudio.c.lima@hotmail.com>
2024-10-18 20:18:22 -0300

    Configurando autenticação JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CreateAccountController } from './controllers/create-account.controller';
 import { envSchema } from './env';
+import { AuthModule } from './modules/auth/auth.module';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
@@ -10,6 +11,7 @@ import { PrismaService } from './prisma/prisma.service';
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+    AuthModule,
   ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
--- src/env.ts
+++ src/env.ts
@@ -5,6 +5,7 @@ export const envSchema = z.object({
   PORT: z.coerce.number().optional().default(3333),
   APP_URL: z.string().url().optional().default('http://surb.com.br'),
   SUPPORT_EMAIL: z.string().optional().default('contact@surb.com.br'),
+  JWT_SECRET: z.string(),
 });
 
 export type Env = z.infer<typeof envSchema>;
